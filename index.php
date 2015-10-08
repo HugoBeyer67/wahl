9d8975cff6448635a28acf2c2901c59a34907416
HugoBeyer67 <hugo.beyer2103@outlook.fr>
2015-10-08 14:31:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="css/knacss.css">
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+
+	</div>
+</body>
+</html>
