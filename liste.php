e4d856a37a99a9ef3bdefa0d77ef6aff2a27f52c
HugoBeyer67 <hugo.beyer2103@outlook.fr>
2015-11-04 11:31:53 +0100

    tableau improvements

---
+++ liste.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Accueil</title>
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<header>
+		<h1><a href="index.php">BANG</a></h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+      <h2>Liste de substitution</a></h2>
+    <ul>
+        <li>Realisateur : David Lynch
+            <ul>
+                <li>Titre du film : Lost Highway</li>
+                <li>Année de réalisation : 1997</li>
+                <li>Genre : Thriller psychologique</li>
+            </ul>
+            <br>
+            <ul>
+                <li>Titre du film : Blue Velvet</li>
+                <li>Année de réalisation : 1987</li>
+                <li>Genre : Thriller</li>
+            </ul>
+        </li>
+    </ul>
+    <a href="index.php#after-tab">Retour apres le tableau</a>
+    <footer>
+	</footer>
+</body>
+</html>
