47921d4a6bab67499e2ef329e28648690edf9943
HugoBeyer67 <hugo.beyer2103@outlook.fr>
2015-11-04 12:09:42 +0100

    add page creation

---
+++ creations.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Créations</title>
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<?php include("include/header.php"); ?>  
+
+	<div class="contenu">
+        Page création  
+	</div>
+	<?php include("include/footer.php"); ?>  
+</body>
+</html>
