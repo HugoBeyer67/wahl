6b56095030356f196156b84e43b606abc1fbae99
HugoBeyer67 <hugo.beyer2103@outlook.fr>
2015-11-08 14:03:36 +0100

    footer content + add pages mentions legales + sitemap (sitemap à faire)

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,13 @@
 <footer>
-  content of footer
+  <div class="footer_left">
+    <ul>
+      <li><a href="sitemap.php">Plan du site</a></li>
+      <li><a href="mentions.php">Mentions légales</a></li>
+    </ul>
+  </div>
+  <div class="footer_right">
+    <p>
+      Copyright BANG corporation 2015
+    </p>
+  </div>
 </footer>
---
+++ sitemap.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Accueil</title>
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/table.js"></script>
+</head>
+<body>
+	<?php include("include/header.php"); ?>  
+
+	<div class="contenu">
+        <div id="main">
+					SITEMAP
+				</div> 
+        <?php include("include/actus.php"); ?>    
+	</div>
+	<?php include("include/footer.php"); ?>  
+</body>
+</html>
