e6e4c479db7c4da580569d8749258232b098b74e
Franz9425 <f.mang@orange.fr>
2015-11-10 16:35:15 +0100

    Corrections mineures + déclaration de confidentialité

--- include/footer.php
+++ include/footer.php
@@ -3,6 +3,7 @@
     <ul>
       <li><a href="sitemap.php">Plan du site</a></li>
       <li><a href="mentions.php">Mentions légales</a></li>
+      <li><a href="declaration.php">Déclaration d'accessibilité</a></li>
     </ul>
   </div>
   <div class="footer_right">
